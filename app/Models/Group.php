1d17e39a8f685de1d0c3b4d75bb96042da39766d
Yuji <hoa8cthcs@gmail.com>
2024-07-06 13:36:48 +0700

    do logic in backend to push and get message of selected conversation

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -68,4 +68,13 @@ class Group extends Model
             'last_message_date' => $this->last_message_date,
         ];
     }
+
+    public static function updateGroupWithMessage($groupId, $message)
+    {
+        // Create or update group with received group id and message
+        return self::updateOrCreate(
+           ['id' => $groupId], // search condition
+           ['last_message_id' => $message->id] // values to update
+        );
+    }
 }

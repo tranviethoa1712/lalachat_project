09dfde595dd9148c1c106dba72c5fd8b0e65f4ed
Yuji <hoa8cthcs@gmail.com>
2024-07-11 20:04:33 +0700

    adding and sending emojis

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="cupcake" class="overflow-hidden h-screen">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="cupcake" class= "overflow-hidden h-screen">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
